a6aac828637d0fee951ee0ae8199431f1e9ab6c5
Katherine Gomez <kathyg0524@gmail.com>
2022-07-04 13:43:26 -0400

    updated data file

---
+++ Data/retirement_titles.csv.sql
@@ -0,0 +1,15 @@
+
+-- Joining titles and employees tables
+SELECT employees.emp_no,
+     employees.first_name,
+	 employees.last_name,
+	 titles.title,
+     titles.from_date,
+     titles.to_date
+FROM employees
+LEFT JOIN titles
+ON employees.emp_no = titles.emp_no
+WHERE (employees.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY employees.emp_no ASC
+;
+
