47c41c3e913f464b8a90dbf31d721e68666a9aaa
Katherine Gomez <kathyg0524@gmail.com>
2022-07-04 15:34:28 -0400

    add mentorship_eligibility data

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,13 +1,22 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (retirement_titles.emp_no) emp_no,first_name, last_name,title
-INTO unique_titles
-FROM retirement_titles
-ORDER BY emp_no ASC, to_date DESC;
+-- mentorship eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+  e.first_name,
+  e.last_name,
+  e.birth_date,
+  de.from_date,
+  de.to_date,
+  ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+  ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+  ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+SELECT * FROM mentorship_eligibility;
 
 CREATE TABLE retirement_titles(
      emp_no INT NOT NULL,
